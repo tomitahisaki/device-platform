ff03e55408ab75273a9f130d260d613fce6b68fb
hisakey <alltimelow2011@gmail.com>
2025-06-07 08:46:51 +0900

    feat: change cron

--- device/setup_cron.sh
+++ device/setup_cron.sh
@@ -4,8 +4,8 @@ SCRIPT_PATH="/home/hisakey/device-platform/device/send_data.py"
 PYTHON_PATH="$(which python3)"
 LOG_PATH="/home/hisakey/send_log.txt"
 
-# cron: 毎日 0, 4, 8, 12, 16, 20 時に実行
-CRON_JOB="0 */4 * * * $PYTHON_PATH $SCRIPT_PATH >> $LOG_PATH 2>&1"
+# cron: 毎日 0, 6, 12, 18時に実行
+CRON_JOB="0 */6 * * * $PYTHON_PATH $SCRIPT_PATH >> $LOG_PATH 2>&1"
 
 # 登録済みか確認
 (crontab -l 2>/dev/null | grep -F "$SCRIPT_PATH") && echo "[INFO] cron はすでに登録済みです。" && exit 0
