97452a614acf2de1119694056e45af6295d3b612
hisakey <alltimelow2011@gmail.com>
2025-07-11 21:40:28 +0900

    feat: validation

--- backend/internal/model/sensor_data.go
+++ backend/internal/model/sensor_data.go
@@ -1,9 +1,22 @@
 package model
 
-import "gorm.io/gorm"
+import (
+  "gorm.io/gorm"
+  "errors"
+)
 
 type SensorData struct {
 	gorm.Model
 	Temperature float64 `json:"temperature"`
 	Humidity    float64 `json:"humidity"`
+}
+
+func ValidateSensorData(sensorData *SensorData) error {
+  if sensorData.Temperature < -100 || sensorData.Temperature > 100 {
+    return errors.New("temperature out of range")
+  }
+  if sensorData.Humidity < 0 || sensorData.Humidity > 100 {
+    return errors.New("humidity out of range")
+  }
+  return nil
 }
---
+++ backend/internal/model/sensor_data_test.go
@@ -0,0 +1,54 @@
+// internal/model/sensor_data_test.go
+package model
+
+import (
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func TestValidateSensorData(t *testing.T) {
+    tests := []struct {
+        name        string
+        input       *SensorData
+        expectedErr string
+    }{
+        {
+            name:        "valid sensor data",
+            input:       &SensorData{Temperature: 25, Humidity: 50},
+            expectedErr: "",
+        },
+        {
+            name:        "temperature too low",
+            input:       &SensorData{Temperature: -150, Humidity: 50},
+            expectedErr: "temperature out of range",
+        },
+        {
+            name:        "temperature too high",
+            input:       &SensorData{Temperature: 150, Humidity: 50},
+            expectedErr: "temperature out of range",
+        },
+        {
+            name:        "humidity too low",
+            input:       &SensorData{Temperature: 25, Humidity: -10},
+            expectedErr: "humidity out of range",
+        },
+        {
+            name:        "humidity too high",
+            input:       &SensorData{Temperature: 25, Humidity: 150},
+            expectedErr: "humidity out of range",
+        },
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            err := ValidateSensorData(tt.input)
+            if tt.expectedErr == "" {
+                assert.NoError(t, err)
+            } else {
+                assert.EqualError(t, err, tt.expectedErr)
+            }
+        })
+    }
+}
+
